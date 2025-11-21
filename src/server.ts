f55e37ff158e6ffde0b731af4630d25bfe995f4b
Naominm <naomimbugua349@gmail.com>
2025-11-21 18:06:18 +0300

    Fix: remove the root route  from server file

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Ticket management system running (Typescript)');
-});
-
 app.use(express.json());
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
