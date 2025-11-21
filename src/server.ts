427f8b9cc3121c23fcae0945998a09add1a315dd
Naominm <naomimbugua349@gmail.com>
2025-11-21 18:08:39 +0300

    Feat: pass the auth signup route at server file

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,9 @@ const app: Express = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
