c262d04bbaa98a2350b4c7fe89480aa8c03551e4
Naominm <naomimbugua349@gmail.com>
2025-11-25 16:28:07 +0300

    Fix: Add cookie parser to fix undefined cookie

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import authRoutes from './routes/authRoutes.js';
 import ticketRoutes from './routes/createTicketRoute.js';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +9,7 @@ const app: Express = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/ticket', ticketRoutes);
