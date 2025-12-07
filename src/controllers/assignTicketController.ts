c3e3ffb34007992323b78eac02c03c15dea90010
Naominm <naomimbugua349@gmail.com>
2025-12-07 11:36:34 +0300

    Feat :Check the role to ensure that only Admin and Agent can reassign a ticket

--- src/controllers/assignTicketController.ts
+++ src/controllers/assignTicketController.ts
@@ -3,6 +3,12 @@ import { Response } from 'express';
 
 export const AssignTicket = (req: AuthRequest, res: Response) => {
   try {
+    const { id } = req.params;
+    const { agentId } = req.body;
+
+    if (req.user.role !== 'AGENT' && req.user.role !== 'ADMIN') {
+      return res.status(403).json({ message: 'Only Admins and agents can reassign tickets' });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Something went wrong Server error', err });
