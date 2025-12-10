de06c4c0bd9b299808840f67a9108418265997f8
Naominm <naomimbugua349@gmail.com>
2025-12-10 18:09:15 +0300

    Faet: Build the comment type to control the type  of te comment

---
+++ prisma/migrations/20251210150711_add/migration.sql
@@ -0,0 +1,5 @@
+-- CreateEnum
+CREATE TYPE "CommentType" AS ENUM ('NOTE', 'ASSIGNMENT', 'REASSIGNMENT', 'STATUS_UPDATE', 'PRIORITY_UPDATE', 'CREATED');
+
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "type" "CommentType" NOT NULL DEFAULT 'NOTE';
