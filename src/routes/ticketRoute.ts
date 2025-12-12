087b722e92e917626776bd932ab298fca7f8ffe0
Naominm <naomimbugua349@gmail.com>
2025-12-12 17:08:34 +0300

    Feat: Route the get Assined to Agent  endpoint

--- src/routes/ticketRoute.ts
+++ src/routes/ticketRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { AssignTicket } from '../controllers/assignTicketController';
+import { getAssignedToAgent } from '../controllers/ticketController';
 import { authMiddleware } from '../middlewares/authmiddleware';
 
 const router = Router();
 router.put('/assign/:id', authMiddleware, AssignTicket);
+router.get('/tickets/assigned/:agentId', authMiddleware, getAssignedToAgent);
 
 export default Router;
