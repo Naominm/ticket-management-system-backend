11ea04693158f417414906710dd3a817f015cd76
Naominm <naomimbugua349@gmail.com>
2026-02-12 11:55:26 +0300

    Feat: Build the profile controller for the user profile

---
+++ src/controllers/profileController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { prisma } from '../prisma';
+import { AuthRequest } from '../middlewares/authmiddleware';
+
+export const getProfile = async (req: AuthRequest, res: Response) => {
+  try {
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
