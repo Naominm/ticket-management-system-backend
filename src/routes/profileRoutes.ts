647cee0320bd7a8f1f961c107dd651c70661b267
Naominm <naomimbugua349@gmail.com>
2026-02-26 14:50:11 +0300

    Feat: Mount the update profile route at profile routes

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getProfile } from '../controllers/profileController';
+import { getProfile,updateProfile } from '../controllers/profileController';
 import { authMiddleware } from '../middlewares/authmiddleware';
 
 const router = Router();
 
 router.get('/', authMiddleware, getProfile);
+router.put("/", authMiddleware, updateProfile);
 
 export default router;
