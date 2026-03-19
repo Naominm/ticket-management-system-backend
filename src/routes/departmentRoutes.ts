cb16f321d3ee877944510179681d514ee0aba824
Naominm <naomimbugua349@gmail.com>
2026-03-19 15:07:05 +0300

    Feat: Protect the Department routes

--- src/routes/departmentRoutes.ts
+++ src/routes/departmentRoutes.ts
@@ -2,10 +2,11 @@
 
 import express from 'express';
 import { getAllDepartments, getDepartmentById } from '../controllers/DepartmentController';
+import { authMiddleware } from '../middlewares/authmiddleware';
 
 const router = express.Router();
 
-router.get('/', getAllDepartments);
-router.get('/:id', getDepartmentById);
+router.get('/', authMiddleware, getAllDepartments);
+router.get('/:id', authMiddleware, getDepartmentById);
 
 export default router;
