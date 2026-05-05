4781ff43d38cf7bb22824ec189664902e5239d76
Naominm <naomimbugua349@gmail.com>
2026-05-05 14:38:05 +0300

    Feat: import the routes endpoints at authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { Login } from '../controllers/authController.js';
+import { authMiddleware } from '../middlewares/authmiddleware.js';
+import { Setup, CreateUser, GetAllUsers, Login } from '../controllers/authController.js';
 
 console.log('Hello from routes');
 
 const router = Router();
 
+router.post('setup', Setup);
 router.post('/login', Login);
+router.post('/users', authMiddleware, CreateUser);
+router.get('/users', authMiddleware, GetAllUsers);
 
 export default router;
