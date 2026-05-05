0677be6f2e8a0a1b526ad509e021490a8b175e43
Naominm <naomimbugua349@gmail.com>
2026-05-05 14:27:22 +0300

    Fix: Remove the signup route from the authRoutes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { Signup, Login } from '../controllers/authController.js';
+import { Login } from '../controllers/authController.js';
 
 console.log('Hello from routes');
 
 const router = Router();
 
-router.post('/signup', Signup);
 router.post('/login', Login);
 
 export default router;
