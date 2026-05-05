f3ae8f09966ad4c13aae7d1d19e717e37556ccb3
Naominm <naomimbugua349@gmail.com>
2026-05-05 14:53:23 +0300

    Feat:Modify the department controller to authenticate the requests

--- src/controllers/DepartmentController.ts
+++ src/controllers/DepartmentController.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from 'express';
 import { prisma } from '../prisma';
+import { AuthRequest } from '../middlewares/authmiddleware';
 
 export const getAllDepartments = async (req: Request, res: Response) => {
   try {
+    const authReq = req as AuthRequest;
+    if (!authReq.user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    if (authReq.user.role !== 'ADMIN') {
+      return res.status(403).json({ message: 'Forbidden.' });
+    }
     const departments = await prisma.department.findMany({
       orderBy: { name: 'asc' },
+      include: {
+        _count: { select: { users: true, tickets: true } },
+      },
     });
 
     res.status(200).json(departments);
   } catch (error) {
-    console.error('Error fetching departments:', error);
     res.status(500).json({ message: 'Failed to fetch departments' });
   }
 };
